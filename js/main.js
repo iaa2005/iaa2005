8555fdb18cfd2b0d59f93e48888a89863871a2bd
iaa2005.home <marsee3015@gmail.com>
2021-04-10 20:49:40 +0300

    deleted require (only for local)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let THREE = require('three');
+// let THREE = require('three');
 let scene = new THREE.Scene()
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 let renderer = new THREE.WebGLRenderer()
