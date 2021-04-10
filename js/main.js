770aecf199fb95f31cfa8499edb8fc1436e5e2bc
iaa2005.home <marsee3015@gmail.com>
2021-04-10 21:00:33 +0300

    added new material

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
-// let THREE = require('three');
+// import * as THREE from 'three'
+
 let scene = new THREE.Scene()
+
+let spotLight = new THREE.SpotLight(0xeeeece)
+spotLight.position.set(1000, 1000, 1000)
+scene.add(spotLight)
+
+let spotLight2 = new THREE.SpotLight(0xffffff);
+spotLight2.position.set( -200, -200, -200);
+scene.add(spotLight2);
+
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 let renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 let geometry = new THREE.BoxGeometry(10, 10, 10)
-let material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00
-})
-let materiaRGB = new THREE.MeshNormalMaterial()
 
-let cube = new THREE.Mesh(geometry, materiaRGB)
+// let material = new THREE.MeshBasicMaterial({
+//     color: 0x00ff00
+// })
+// let materialRGB = new THREE.MeshNormalMaterial()
+
+let material = new THREE.MeshPhongMaterial({
+    color: 0xffffff,
+    specular: 0x234355
+})
+
+let cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
 camera.position.z = 25
@@ -22,4 +38,5 @@ function render() {
     cube.rotation.y += 0.01  
     renderer.render(scene, camera)
 }
+
 render()
